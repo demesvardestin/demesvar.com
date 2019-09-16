ca3683d0bcd73c2a370f239b888528a970e70dca
Ubuntu <ubuntu@ip-172-31-88-60.ec2.internal>
2019-09-16 23:49:51 +0000

    commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
     def home
         @featured_post = Article.last # for now, will replace later with piece of code
-        @latest_posts = Article.last(4) # for now, will replace with arbitrary number
+        @latest_posts = Article.where(project_id: nil).last(4) # for now, will replace with arbitrary number
         @latest_projects = Project.last(5)
     end
 end
