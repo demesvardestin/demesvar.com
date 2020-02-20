dd079f7b5d496bd01fc89475a6dbded70c6d3e17
Ubuntu <ubuntu@ip-172-31-88-60.ec2.internal>
2020-02-20 21:31:17 +0000

    commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
     def home
-        @latest_posts = Article.published.where(project_id: nil).order("created_at DESC").last(4)
+        @latest_posts = Article.published.where(project_id: nil).order("created_at DESC").last(10)
         @latest_projects = Project.all.order("created_at DESC").last(5)
     end
 end
