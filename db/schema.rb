a0e9bb8c9f88b29ef6a87d39ff6d18efdb36ef0e
Ubuntu <ubuntu@ip-172-31-13-33.us-east-2.compute.internal>
2020-04-22 01:33:17 +0000

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190915210443) do
+ActiveRecord::Schema.define(version: 20200422012650) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "first_name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20190915210443) do
     t.integer  "admin_id"
     t.string   "category_name"
     t.boolean  "published",     default: true
+    t.text     "description"
   end
 
   create_table "categories", force: :cascade do |t|
