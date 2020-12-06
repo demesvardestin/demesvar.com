e98727dbf3159cdb922df4fab1affb15299096a0
Ubuntu <ubuntu@ip-172-31-44-246.us-east-2.compute.internal>
2020-12-06 18:04:49 +0000

    commit

--- app/controllers/fitness_controller.rb
+++ app/controllers/fitness_controller.rb
@@ -1,6 +1,6 @@
 class FitnessController < ApplicationController
     def index
-        @article = Article.where("tags LIKE ?", "fitness").last
+        @article = Article.where("tags LIKE %fitness%").last
         
         if !@article.nil?
             redirect_to blog_show_article_path(:slug => @article.slug, :id => @article.id)
